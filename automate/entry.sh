4ad5dad2e70c604fba89aa99d8eb692454452309
sheetalgcin <42903527+sheetalgcin@users.noreply.github.com>
2023-11-05 19:41:44 +0100

    Update entry.sh

--- automate/entry.sh
+++ automate/entry.sh
@@ -2,7 +2,7 @@
 
 # Set and export environment variables based on conditions
 
-print($x)
+echo "MY_VARIABLE is: $x"
 # Run the Robot Framework tests with the chosen environment variables
 robot --variable x:$x --variable y:$y -d  output --output  output_$x.xml  --xunit  res.xml  Tests/verify/*
 #robot  test.robot  # Replace with the actual path to your Robot Framework tests
